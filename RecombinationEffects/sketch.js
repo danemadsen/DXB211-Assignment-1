38977508a2b74aa8e7f49e3260ad63721c9070e4
Dane Madsen <dane_madsen@hotmail.com>
2023-03-28 07:08:18 +1000

    Improved RecombinationEffects

--- RecombinationEffects/sketch.js
+++ RecombinationEffects/sketch.js
@@ -9,7 +9,19 @@ function setup(){
   pixelDensity(1); // set pixel density to 1
   createCanvas(img.width, img.height);
   background(0);
+}
 
+function draw(){
+  // if left mouse button is pressed, increase pixel size
+  // if left mouse button + ctrl is pressed, decrease pixel size
+  if(mouseIsPressed){
+    if(keyIsDown(CONTROL)){
+      pixelsize -= 1;
+    } else {
+      pixelsize += 1;
+    }
+  }
+  
   for(let i = 0; i < width; i += pixelsize){
     for(let j = 0; j < height; j += pixelsize){
       let pixColor = img.get(i, j);   
